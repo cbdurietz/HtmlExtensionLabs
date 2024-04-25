fb4e089c10005037cb260c939582d48ed733c333
CB Du Rietz <cbdurietz@gmail.com>
2024-04-25 14:56:24 +0200

    Fixed some minor issues.

--- Taggarna.HtmlTagHelpers/InlineSvgTagHelper.cs
+++ Taggarna.HtmlTagHelpers/InlineSvgTagHelper.cs
@@ -6,8 +6,10 @@ using System.Xml.Linq;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
 
-namespace Taggarna.HtmlTagHelpers {
-    public class InlineSvgTagHelper : TagHelper {
+namespace Taggarna.HtmlTagHelpers
+{
+    public class InlineSvgTagHelper : TagHelper
+    {
 
         public string IconName { get; set; } = string.Empty;
         public string? Height { get; set; }
@@ -23,26 +25,31 @@ namespace Taggarna.HtmlTagHelpers {
         private readonly bool _debug; // = true; //TODO: Should be set up to fetch from appsettings.
 
 
-        public InlineSvgTagHelper(IConfiguration configuration, IMemoryCache memoryCache) {
+        public InlineSvgTagHelper(IConfiguration configuration, IMemoryCache memoryCache)
+        {
             _configuration = configuration;
             _memoryCache = memoryCache;
             _iconStoreFolder = _configuration["InlineSvgTagHelper:SvgFilePath"];
             _debug = Convert.ToBoolean(_configuration["InlineSvgTagHelper:Debug"]);
         }
 
-        public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output) {
-            try {
+        public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            try
+            {
                 var timer = new Stopwatch();
                 timer.Start();
 
                 // Check if the IconName is valid.
-                if (!IsValidIconName(IconName)) {
+                if (!IsValidIconName(IconName))
+                {
                     throw new ArgumentException($"Invalid icon name: {IconName}");
                 }
 
                 // If the requested svg is already loaded into the cache, use that. Otherwise, load it from file.
                 var source = SvgSource.Cache;
-                if (!_memoryCache.TryGetValue($"InlineSvgTagHelper_{IconName}", out HtmlDocument svgDocument)) {
+                if (!_memoryCache.TryGetValue($"InlineSvgTagHelper_{IconName}", out HtmlDocument svgDocument))
+                {
                     svgDocument = CreateHtmlDocument(IconName);
 
                     var cacheEntryOptions = new MemoryCacheEntryOptions()
@@ -52,11 +59,14 @@ namespace Taggarna.HtmlTagHelpers {
                     source = SvgSource.File;
                 }
 
-                var svgRoot = svgDocument.DocumentNode.SelectSingleNode("//svg");
-                var svgContent = svgRoot.WriteContentTo();
-                foreach (var itemAttribute in svgRoot.Attributes) {
-                    output.Attributes.SetAttribute(itemAttribute.Name, itemAttribute.Value);
-                }
+                var svgRoot = svgDocument?.DocumentNode.SelectSingleNode("//svg");
+                var svgContent = svgRoot?.WriteContentTo();
+                if (svgRoot?.Attributes != null)
+                    foreach (var itemAttribute in svgRoot.Attributes)
+                    {
+                        output.Attributes.SetAttribute(itemAttribute.Name, itemAttribute.Value);
+                    }
+
                 output.Content.SetHtmlContent(svgContent);
 
                 timer.Stop();
@@ -79,10 +89,12 @@ namespace Taggarna.HtmlTagHelpers {
                 Console.Write("ticks).");
                 Console.WriteLine();
 
-            } catch (Exception e) {
+            } catch (Exception e)
+            {
                 Height = 0.ToString();
                 Width = 0.ToString();
-                if (_debug) {
+                if (_debug)
+                {
                     output.Attributes.SetAttribute("Exception", e.Message);
                 }
                 Console.ForegroundColor = ConsoleColor.White;
@@ -90,37 +102,46 @@ namespace Taggarna.HtmlTagHelpers {
                 Console.ForegroundColor = ConsoleColor.Gray;
                 Console.Write($" : {e.Message}");
                 Console.WriteLine();
-            } finally {
+            } finally
+            {
                 output.TagName = "svg";
                 output.TagMode = TagMode.StartTagAndEndTag;
-                if (Height is not null) {
+                if (Height is not null)
+                {
                     output.Attributes.SetAttribute("height", Height);
                 }
-                if (Width is not null) {
+                if (Width is not null)
+                {
                     output.Attributes.SetAttribute("width", Width);
                 }
-                if (Style is not null) {
+                if (Style is not null)
+                {
                     output.Attributes.SetAttribute("style", Style);
                 }
-                if (Class is not null) {
+                if (Class is not null)
+                {
                     output.Attributes.SetAttribute("class", Class);
                 }
             }
             return base.ProcessAsync(context, output);
         }
 
-        private HtmlDocument CreateHtmlDocument(string iconName) {
+        private HtmlDocument CreateHtmlDocument(string iconName)
+        {
             var iconFilePath = Path.Combine(AppContext.BaseDirectory, _iconStoreFolder, iconName + ".svg");
             var doc = new HtmlDocument();
 
-            try {
+            try
+            {
                 doc.Load(iconFilePath);
 
                 // Validate the svg file.
-                if (!IsValidSvg(doc.ParsedText)) {
+                if (!IsValidSvg(doc.ParsedText))
+                {
                     throw new XmlException($"The loaded file is not a valid svg file. (File '{iconName}')");
                 }
-            } catch (Exception e) {
+            } catch (Exception e)
+            {
                 Console.WriteLine(e);
                 throw;
             }
@@ -128,24 +149,30 @@ namespace Taggarna.HtmlTagHelpers {
 
         }
 
-        private bool IsValidIconName(string iconName) {
+        private bool IsValidIconName(string iconName)
+        {
             // If there's no IconName input, throw an exception.
-            if (string.IsNullOrEmpty(iconName)) {
+            if (string.IsNullOrEmpty(iconName))
+            {
                 return false;
             }
             // If trying to traverse outside of the defined Icon Store, throw an exception.
             return !iconName.Contains("..");
         }
-        private bool IsValidSvg(string str) {
-            try {
+        private bool IsValidSvg(string str)
+        {
+            try
+            {
                 var svg = XDocument.Load(new StringReader(str));
                 return svg.Root != null && svg.Root.Name.LocalName.Equals("svg");
-            } catch (Exception e) {
+            } catch (Exception)
+            {
                 return false;
             }
         }
 
-        private enum SvgSource {
+        private enum SvgSource
+        {
             File = 1,
             Cache = 2
         }
