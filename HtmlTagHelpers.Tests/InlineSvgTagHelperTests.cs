1a118490bb7583bdf1ca9a8bf5a3725f68032e93
CB Du Rietz <cbdurietz@gmail.com>
2024-04-25 11:39:50 +0200

    Removed some unused variables in the test project.

--- HtmlTagHelpers.Tests/InlineSvgTagHelperTests.cs
+++ HtmlTagHelpers.Tests/InlineSvgTagHelperTests.cs
@@ -1,29 +1,26 @@
 ﻿namespace HtmlTagHelpers.Tests {
     public class InlineSvgTagHelperTests {
 
-        //private IConfiguration _configuration;
-        //private IMemoryCache _memoryCache;
-
-        //public InlineSvgTagHelperTests(IConfiguration configuration, IMemoryCache memoryCache) {
-        //    _configuration = configuration;
-        //    _memoryCache = memoryCache;
-        //}
-
         [Fact]
         public void Test01() {
             // Arrange
 
             // Mock the Configuration.
-            var config = new Mock<IConfiguration>();
-            config.Object["InlineSvgTagHelper:SvgFilePath"] = "Assets/Icons";
-            config.Object["InlineSvgTagHelper:Debug"] = "true";
+            var config = new Mock<IConfiguration>
+            {
+                Object =
+                {
+                    ["InlineSvgTagHelper:SvgFilePath"] = "Assets/Icons",
+                    ["InlineSvgTagHelper:Debug"] = "true"
+                }
+            };
 
             // Mock the TagHelperContext
             var tagHelperContext = new TagHelperContext(
                 new TagHelperAttributeList(),
                 new Dictionary<object, object>(),
                 Guid.NewGuid().ToString("N"));
-            var tagHelperOutput = new TagHelperOutput("inlinesvg", new TagHelperAttributeList(), (result, encoder) => {
+            var tagHelperOutput = new TagHelperOutput("inlinesvg", new TagHelperAttributeList(), (_, _) => {
                 var tagHelperContent = new DefaultTagHelperContent();
                 tagHelperContent.SetHtmlContent(string.Empty);
                 return Task.FromResult<TagHelperContent>(tagHelperContent);
@@ -32,22 +29,19 @@
             // Mock the Cache
             var mockCache = new Mock<IMemoryCache>();
             var mockCacheEntry = new Mock<ICacheEntry>();
-            string? keyPayload = null;
+
             mockCache
                 .Setup(mc => mc.CreateEntry(It.IsAny<object>()))
-                .Callback((object k) => keyPayload = (string)k)
+                .Callback((object k) => _ = (string)k)
                 .Returns(mockCacheEntry.Object);
 
-            object? valuePayload = null;
             mockCacheEntry
                 .SetupSet(mce => mce.Value = It.IsAny<object>())
-                .Callback<object>(v => valuePayload = v);
+                .Callback<object>(v => _ = v);
 
-            TimeSpan? expirationPayload = null;
             mockCacheEntry
                 .SetupSet(mce => mce.AbsoluteExpirationRelativeToNow = It.IsAny<TimeSpan?>())
-                .Callback<TimeSpan?>(dto => expirationPayload = dto);
-
+                .Callback<TimeSpan?>(dto => _ = dto);
 
             var sut = new InlineSvgTagHelper(config.Object, mockCache.Object) {
                 IconName = "something",
